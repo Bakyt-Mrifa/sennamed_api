7180274329b13fddee3c8a2d42b66ec4c85962b0
Мамбеталиев Бакыт Мэлисович <bmambetaliev@megacom.local>
2021-05-18 17:52:39 +0600

    api done

--- src/main/java/kg/sennamed/sennamed_api/controllers/LoginController.java
+++ src/main/java/kg/sennamed/sennamed_api/controllers/LoginController.java
@@ -1,12 +1,11 @@
 package kg.sennamed.sennamed_api.controllers;
 
+import io.swagger.annotations.Api;
 import kg.sennamed.sennamed_api.models.dto.AccountDto;
 import kg.sennamed.sennamed_api.models.responses.Responses;
 import kg.sennamed.sennamed_api.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import io.swagger.annotations.Api;
 
 @RestController
 @RequestMapping(value = "api/v1/login/")
@@ -22,6 +21,14 @@ public class LoginController {
         System.out.println(accountDto);
        return loginService.getAccount(accountDto);
 
+    }
+
+    @PostMapping(value = "test")
+    //public void testReceive (@RequestParam String login, @RequestParam String password){
+    public Responses testReceive (@RequestBody AccountDto accountDto){
+        System.out.println("login = "+accountDto.getLogin()+", password = "+accountDto.getPassword());
+        System.out.println(accountDto);
+        return null;
     }
 
 }
